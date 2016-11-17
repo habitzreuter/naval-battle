56d69dcb6905f284b8008e92ef081699581bbbcf
Marco Antônio Habitzreuter <marco.habitzreuter@ufrgs.br>
2016-11-17 20:51:07 -0200

    Board stores the ship index instead of it's identification code

--- game.c
+++ game.c
@@ -13,11 +13,17 @@
 #include "player.h"
 #include "ai.h"
 
+// Constantes de tamanhos máximos de vetores
 #define MAX_BOARD_SIZE	100
 #define MAX_SHIPS	12
 #define MAX_NAME_SIZE	31
 
+// Constantes do tabuleiro do jogador
+#define NO_SHIP		250
+
+// Constantes do tabuleiro do adversário
 #define MISSED_SHOT	255
+#define NOT_SHOT	NO_SHIP
 
 #define HUMAN	true
 #define AI	false
@@ -31,7 +37,8 @@ enum ships {
 
 typedef struct {
 	size_t size; // Tamanho e tipo do navio
-	uint8_t initial_row, initial_column;
+	uint8_t initial_row, initial_column,
+		hits; // Quantos tiros atingiram o navio
 	bool direction; // 1: vertical, 0: horizontal
 } ship_st;
 
@@ -50,13 +57,45 @@ typedef struct {
 	player_st player1, player2;
 } game_st;
 
+/**
+ * Retorna string correspondente ao navio
+ */
+void stringify_ship_type(enum ships type, char *destination)
+{
+	switch(type) {
+		case 5:
+			strcpy(destination, "Porta Aviões");
+			break;
+		case 4:
+			strcpy(destination, "Destroyer");
+			break;
+		case 2:
+			strcpy(destination, "Cruzador");
+			break;
+		case 1:
+			strcpy(destination, "Submarino");
+			break;
+	}
+}
+
+/**
+ * Retorna caracere correspondente ao navio
+ */
+char get_ship_type(enum ships type)
+{
+	char tmp[] = "String that will contain the ship name";
+	char *c = tmp;
+	stringify_ship_type(type, c);
+	return c[0];
+}
+
 /*
  * Imprime tabuleiro na tela
  */
-void print_board(size_t size, uint8_t *board)
+void print_board(size_t size, uint8_t *board, ship_st ships[MAX_SHIPS])
 {
 	char c;
-	uint8_t content;
+	uint8_t index;
 	for(uint8_t i = 0; i < size + 1; i++) {
 
 		// Imprime número da linha
@@ -64,55 +103,29 @@ void print_board(size_t size, uint8_t *board)
 		else printf("       ");
 
 		for(uint8_t j = 0; j < size; j++) {
-			if(i == 0) {
-				// Imprime letra da coluna na primeira linha
-				printf("%2c ", (65 + j));
-			} else {
-				content = *(board + MAX_BOARD_SIZE * (i - 1) + j);
-				switch (content) {
-				case 5:
-					c = 'P';
-					break;
-				case 4:
-					c = 'D';
-					break;
-				case 2:
-					c = 'C';
-					break;
-				case 1:
-					c = 'S';
-					break;
-				case 0:
+			// Imprime letra da coluna na primeira linha
+			if(i == 0) printf("%2c ", (65 + j));
+			else {
+				// Le valor na célula do tabuleiro
+				index = *(board + MAX_BOARD_SIZE * (i - 1) + j);
+
+				// Verifica os códigos de status
+				switch(index) {
+				case NO_SHIP:
 					c = '~';
 					break;
 				case MISSED_SHOT:
 					c = 'X';
+					break;
+				default:
+					// Não é código de status, então é navio
+					c = get_ship_type(ships[index].size);
 				}
+
 				printf("%2c ", c);
 			}
 		}
 		printf("\n");
-	}
-}
-
-/**
- * Retorna string correspondente ao número do navio
- */
-void stringify_ship_type(enum ships type, char *destination)
-{
-	switch(type) {
-	case 5:
-		strcpy(destination, "Porta Aviões");
-		break;
-	case 4:
-		strcpy(destination, "Destroyer");
-		break;
-	case 2:
-		strcpy(destination, "Cruzador");
-		break;
-	case 1:
-		strcpy(destination, "Submarino");
-		break;
 	}
 }
 
@@ -136,7 +149,7 @@ bool ship_superposition(size_t ship_size, uint8_t *board, uint8_t row,
 			cell_content = *(board + MAX_BOARD_SIZE * (row + i) + col);
 			break;
 		}
-		if(cell_content != 0) empty_cell--;
+		if(cell_content != NO_SHIP) empty_cell--;
 	}
 
 	if(empty_cell == ship_size) status = false;
@@ -205,18 +218,18 @@ void convert_coords_to_index(char tmp_col, uint16_t tmp_row, uint8_t *col, uint8
 /**
  * Insere navio no tabuleiro
  */
-void update_board(uint8_t *board, ship_st ship)
+void update_board(uint8_t *board, ship_st ship, uint8_t ship_index)
 {
 	uint8_t in_row = ship.initial_row, in_col = ship.initial_column;
 
 	switch(ship.direction) {
 	case 0:
 		for(uint8_t i = 0; i < ship.size; i++)
-			*(board + MAX_BOARD_SIZE * in_row + in_col + i) = ship.size;
+			*(board + MAX_BOARD_SIZE * in_row + in_col + i) = ship_index;
 		break;
 	case 1:
 		for(uint8_t i = 0; i < ship.size; i++)
-			*(board + MAX_BOARD_SIZE * (in_row + i) + in_col) = ship.size;
+			*(board + MAX_BOARD_SIZE * (in_row + i) + in_col) = ship_index;
 		break;
 	}
 }
@@ -226,7 +239,7 @@ void update_board(uint8_t *board, ship_st ship)
  */
 void set_ships(player_st *player, size_t board_size, bool human)
 {
-	uint8_t ship_count = sizeof(player->ships) / sizeof(ship_st);
+	uint8_t ship_count = MAX_SHIPS;
 	char ship_name[20];
 
 	for(uint8_t i = 0; i < ship_count; i++) {
@@ -235,7 +248,7 @@ void set_ships(player_st *player, size_t board_size, bool human)
 		do {
 			if(human) {
 				stringify_ship_type(ship->size, ship_name);
-				print_board(board_size, &(player->board[0][0]));
+				print_board(board_size, &(player->board[0][0]), player->ships);
 
 				printf("Coordenada inicial para o %s (linha, coluna e direção): ", ship_name);
 				scan_ship_position(
@@ -250,7 +263,7 @@ void set_ships(player_st *player, size_t board_size, bool human)
 
 		} while(!valid_position(board_size, &(player->board[0][0]), *ship));
 
-		update_board(&(player->board[0][0]), *ship);
+		update_board(&(player->board[0][0]), *ship, i);
 	}
 }
 
@@ -260,25 +273,36 @@ void set_ships(player_st *player, size_t board_size, bool human)
  */
 game_st set_default_values()
 {
-	ship_st aircraft_carrier = {5, -1, -1, 0};
-	ship_st destroyer = {4, -1, -1, 0};
-	ship_st cruzer= {2, -1, -1, 0};
-	ship_st submarine = {1, -1, -1, 0};
+	ship_st aircraft_carrier = {5, 0, 0, 0, 0};
+	ship_st destroyer = {4, 0, 0, 0, 0};
+	ship_st cruzer= {2, 0, 0, 0, 0};
+	ship_st submarine = {1, 0, 0, 0, 0};
 	
-	player_st player = {"Jogador", 0, 15, {
-		aircraft_carrier,
-		destroyer,
-		destroyer,
-		cruzer,
-		cruzer,
-		cruzer,
-		cruzer,
-		submarine,
-		submarine,
-		submarine,
-		submarine,
-		submarine
-	}, {{0}}, {{0}}};
+	player_st player;
+	player.score = 0;
+	player.ammo = 15;
+
+	// Inicializa tabuleiros
+	for(uint8_t i = 0; i < MAX_BOARD_SIZE; i++) {
+		for(uint8_t j = 0; j < MAX_BOARD_SIZE; j++) {
+			player.board[i][j] = NO_SHIP;
+			player.enemy_board[i][j] = NOT_SHOT;
+		}
+	}
+
+	// Inicializa navios
+	player.ships[0] = aircraft_carrier;
+	player.ships[1] = destroyer;
+	player.ships[2] = destroyer;
+	player.ships[3] = cruzer;
+	player.ships[4] = cruzer;
+	player.ships[5] = cruzer;
+	player.ships[6] = cruzer;
+	player.ships[7] = submarine;
+	player.ships[8] = submarine;
+	player.ships[9] = submarine;
+	player.ships[10] = submarine;
+	player.ships[11] = submarine;
 
 	game_st game = {0, 15, player, player};
 	return game;
@@ -289,7 +313,7 @@ game_st set_default_values()
  */
 void shot_try(size_t board_size, player_st *player, player_st *enemy, bool human)
 {
-	uint8_t row, col;
+	uint8_t row, col, index;
 
 	// Le e valida coordenadas
 	do {
@@ -301,11 +325,19 @@ void shot_try(size_t board_size, player_st *player, player_st *enemy, bool human
 
 	} while(!valid_coordinates(board_size, row, col));
 
-	if(enemy->board[row][col] != 0) { // Caso atinja um navio inimigo
+	if(enemy->board[row][col] != NO_SHIP) { // Caso atinja um navio inimigo
 		player->enemy_board[row][col] = enemy->board[row][col];
+		index = enemy->board[row][col];
+		enemy->ships[index].hits++;
+
 		// Se cada tiro gasta uma munição e cada acerto dá duas, então
 		// cada acerto soma uma munição
 		player->ammo++;
+
+		// Recebe 3 munições extra se navio foi completamente destruído
+		if(enemy->ships[index].hits == enemy->ships[index].size)
+			player->ammo += 3;
+
 	} else {
 		// Se errou o tiro, armazena que não há navio nessa posição
 		player->enemy_board[row][col] = MISSED_SHOT;
@@ -340,7 +372,8 @@ void game_new()
 	do {
 		shot_try(game.board_size, &game.player1, &game.player2, HUMAN);
 		shot_try(game.board_size, &game.player2, &game.player1, AI);
-		print_board(game.board_size, &(game.player1.enemy_board[0][0]));
+		print_board(game.board_size, &(game.player2.board[0][0]), game.player2.ships);
+		print_board(game.board_size, &(game.player1.enemy_board[0][0]), game.player1.ships);
 		winner = game_end(&game.player1, &game.player2);
 	} while(winner == 0);
 
