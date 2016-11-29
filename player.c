8a023270cfa81de42de3c997c5526d28d3b34d50
Marco Antônio Habitzreuter <marco.habitzreuter@ufrgs.br>
2016-11-29 21:15:23 -0200

    Moved user input to new function

--- player.c
+++ player.c
@@ -14,12 +14,42 @@
 #include "ships.h"
 
 /**
- * Lê coordenadas e direção para posicionar navio
+ * Le entrada do usuário e executa ação correspondente no tabuleiro
+ */
+bool ship_position_input(ship_st *ship)
+{
+	int c;
+	c = getch();
+	switch(c) {
+	case KEY_DOWN:
+		ship->initial_row++;
+		break;
+	case KEY_UP:
+		ship->initial_row--;
+		break;
+	case KEY_LEFT:
+		ship->initial_column--;
+		break;
+	case KEY_RIGHT:
+		ship->initial_column++;
+		break;
+	case 'd':
+		ship->direction = !ship->direction;
+		break;
+	case 10: // ENTER
+		return true;
+		break;
+	}
+
+	return false;
+}
+
+/**
+ * Chama função de leitura de coordenada e faz a consistência da leitura
  */
 void scan_ship_position(WINDOW *board, WINDOW *info, player_st *player, size_t board_size, uint8_t ship_index)
 {
 	player_st tmp_player; // Tabuleiro temporário, apenas para exibição
-	int c;
 	bool end, valid_pos, valid_bounds, valid_coord;
 	ship_st *ship = &(player->ships[ship_index]);
 	ship_st tmp_ship; // Para validação da entrada
@@ -29,31 +59,18 @@ void scan_ship_position(WINDOW *board, WINDOW *info, player_st *player, size_t b
 		update_board(&(tmp_player.board[0][0]), *ship, ship_index);
 		print_player_board(board, board_size, tmp_player);
 		valid_pos = valid_position(board_size, &(player->board[0][0]), *ship);
-		c = getch();
 		tmp_ship = *ship;
-		switch(c) {
-		case KEY_DOWN:
-			tmp_ship.initial_row++;
-			break;
-		case KEY_UP:
-			tmp_ship.initial_row--;
-			break;
-		case KEY_LEFT:
-			tmp_ship.initial_column--;
-			break;
-		case KEY_RIGHT:
-			tmp_ship.initial_column++;
-			break;
-		case 'd':
-			tmp_ship.direction = !tmp_ship.direction;
-			break;
-		case 10: // ENTER
-			end = true;
-			break;
-		}
+
+		end = ship_position_input(&tmp_ship);
+
 		valid_bounds = valid_ship_bounds(board_size, tmp_ship);
 		valid_coord = valid_coordinates(board_size, tmp_ship.initial_row, tmp_ship.initial_column);
 		if(valid_bounds && valid_coord) *ship = tmp_ship;
+		else if(end) {
+			mvwprintw(info, 1, 1, "Inválido!");
+			wrefresh(info);
+			refresh();
+		}
 	} while(!end || !valid_pos);
 }
 
