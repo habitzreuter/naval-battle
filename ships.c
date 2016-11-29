6186398126e2155022211d5664dd291641fdf341
Marco Antônio Habitzreuter <marco.habitzreuter@ufrgs.br>
2016-11-29 20:58:42 -0200

    Fixed switch/case identation

--- ships.c
+++ ships.c
@@ -21,18 +21,18 @@
 void stringify_ship_type(enum ships type, char *destination)
 {
 	switch(type) {
-		case 5:
-			strcpy(destination, "Porta Aviões");
-			break;
-		case 4:
-			strcpy(destination, "Destroyer");
-			break;
-		case 2:
-			strcpy(destination, "Cruzador");
-			break;
-		case 1:
-			strcpy(destination, "Submarino");
-			break;
+	case 5:
+		strcpy(destination, "Porta Aviões");
+		break;
+	case 4:
+		strcpy(destination, "Destroyer");
+		break;
+	case 2:
+		strcpy(destination, "Cruzador");
+		break;
+	case 1:
+		strcpy(destination, "Submarino");
+		break;
 	}
 }
 
@@ -60,12 +60,12 @@ bool ship_superposition(size_t ship_size, uint8_t *board, uint8_t row,
 
 	for(uint8_t i = 0; i < ship_size; i++) {
 		switch(direction) {
-			case 0:
-				cell_content = *(board + MAX_BOARD_SIZE * row + col + i);
-				break;
-			case 1:
-				cell_content = *(board + MAX_BOARD_SIZE * (row + i) + col);
-				break;
+		case 0:
+			cell_content = *(board + MAX_BOARD_SIZE * row + col + i);
+			break;
+		case 1:
+			cell_content = *(board + MAX_BOARD_SIZE * (row + i) + col);
+			break;
 		}
 		if(cell_content != NO_SHIP) empty_cell--;
 	}
@@ -85,16 +85,16 @@ bool valid_ship_bounds(size_t board_size, ship_st ship)
 	uint8_t final_row, final_col;
 
 	switch(ship.direction) {
-		case 1:
-			final_col = ship.initial_column;
-			final_row = ship.initial_row + ship.size;
-			if(final_row <= board_size) status = true;
-			break;
-		case 0:
-			final_row = ship.initial_row;
-			final_col = ship.initial_column + ship.size;
-			if(final_col <= board_size) status = true;
-			break;
+	case 1:
+		final_col = ship.initial_column;
+		final_row = ship.initial_row + ship.size;
+		if(final_row <= board_size) status = true;
+		break;
+	case 0:
+		final_row = ship.initial_row;
+		final_col = ship.initial_column + ship.size;
+		if(final_col <= board_size) status = true;
+		break;
 	}
 
 	return status;
@@ -123,14 +123,14 @@ void update_board(uint8_t *board, ship_st ship, uint8_t ship_index)
 	uint8_t in_row = ship.initial_row, in_col = ship.initial_column;
 
 	switch(ship.direction) {
-		case 0:
-			for(uint8_t i = 0; i < ship.size; i++)
-				*(board + MAX_BOARD_SIZE * in_row + in_col + i) = ship_index;
-			break;
-		case 1:
-			for(uint8_t i = 0; i < ship.size; i++)
-				*(board + MAX_BOARD_SIZE * (in_row + i) + in_col) = ship_index;
-			break;
+	case 0:
+		for(uint8_t i = 0; i < ship.size; i++)
+			*(board + MAX_BOARD_SIZE * in_row + in_col + i) = ship_index;
+		break;
+	case 1:
+		for(uint8_t i = 0; i < ship.size; i++)
+			*(board + MAX_BOARD_SIZE * (in_row + i) + in_col) = ship_index;
+		break;
 	}
 }
 
